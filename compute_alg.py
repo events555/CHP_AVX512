e31e746a007b331da36f5297b2c7525b7a471fa5
Steven Nguyen <03stevennguyen@gmail.com>
2023-09-12 11:13:13 -0400

    Initial commit

---
+++ compute_alg.py
@@ -0,0 +1,113 @@
+import numpy as np
+import mats
+import os
+import math
+
+SUPPRESS_OUTPUT = True
+RUN_ROUNDS_FROM_1_TO_N = True
+LOG_OUTPUT = True
+imag_unit = np.complex(0+1j)
+
+def qudit_stab(qudits, gates, rounds):
+    #initialize dict
+    vectors = {}
+    for q in qudits:
+        vectors[q.tobytes()] = np.copy(q)
+    
+    for r in range(rounds):
+        for p in gates:
+            curr_vectors = list(vectors.values())
+            for v in curr_vectors:
+                vec = np.matmul(p, v)
+                vectors[vec.tobytes()] = np.copy(vec)
+
+    return vectors
+
+#much slower than using dict, but you can use allclose for matrix comparison
+def qudit_stab_list(qudits, gate_seq, gates, symb, rounds):
+
+    vectors = []
+    d = qudits[0].shape[0]
+
+    for q in qudits:
+        vectors.append(q)
+
+    for r in range(rounds):
+        curr_vectors = vectors.copy()
+        for vec_ind, v in enumerate(curr_vectors):
+             for gate_ind, g in enumerate(gates):
+                 vec = np.matmul(g, v)
+                 not_close = True
+                 omega = np.exp((-2 * math.pi * imag_unit) / (2 * d))
+                 for k in range(2 * d):
+                     if any( [ np.allclose(u, np.power(omega, k) * vec) for u in vectors ] ):
+                         not_close = False
+                         break
+
+                 if not_close:
+                     vectors.append(vec)
+                     gate_seq.append(symb[gate_ind] + gate_seq[vec_ind]) 
+    
+    return [vectors, gate_seq]
+       
+
+
+def qudit_comp_basis(d):
+    indicator = lambda n, k: [1+0j] if k == n else [0+0j]
+    return np.array([ [indicator(dd, ddd) for ddd in range(d)] for dd in range(d)])
+
+
+if __name__ == "__main__":
+    
+    #sanitize input later
+    d = int(input("Dim: "))
+    
+    #gates = mats.pauli_matrices if d == 2 else mats.chp_3[:7]
+    
+    #initialization 
+    gates, symb = mats.chp_plus_labels(d)
+    #ignore CNOT/SUM for now
+    symb = symb[1:]
+    gates = gates[1:]
+    gate_seq = ["|" + str(k) + ">" for k in range(d)]
+    prev_gates = 0
+    stab = qudit_comp_basis(d)
+    rounds_needed = 0
+    #initialization
+
+    while True:
+        stab, gate_seq = qudit_stab_list(stab, gate_seq, gates, symb, 1)
+        rounds_needed += 1
+
+        if not SUPPRESS_OUTPUT:
+            for v in stab:
+                print(v)
+                print()
+                
+        num_gates = len(stab)
+        
+        if (prev_gates == num_gates):
+
+            print("Total gates seen: " + str(num_gates))
+            if LOG_OUTPUT:
+                fname = "d_" + str(d) + "_stabilized_states.txt"
+
+                if os.path.exists(fname):
+                    os.remove(fname)
+
+                fp = open(fname, "w")
+                
+                for v_ind, v in enumerate(stab):
+                    fp.write(str(gate_seq[v_ind]) + "\n")
+                    fp.write(str(v) + "\n\n")
+
+                print("Wrote gates to " + str(fname))
+                fp.close()
+
+            break
+
+        prev_gates = num_gates
+        print("The number of seen vectors: " + str(len(stab)))
+        print()
+
+    print("Successfully terminated in " + str(rounds_needed) + " rounds.")
---
+++ mats.py
@@ -0,0 +1,69 @@
+import numpy as np
+import math
+
+imag_unit = np.complex(0+1j)
+
+pauli_matrices =                                                        \
+        [                                                               \
+                np.array([[0+0j, 1+0j], [1+0j, 0+0j]]),                 \
+                np.array([[0,0-1j], [0+1j, 0]]),                        \
+                np.array([[1+0j, 0+0j], [0+0j, -1+0j]])                 \
+        ]
+
+
+gell_mann_matrices = \
+        [
+            np.array([ [0+0j, 1+0j, 0+0j], [1+0j, 0+0j, 0+0j], [0+0j, 0+0j, 0+0j] ]),           \
+            np.array([ [0+0j, 0-1j, 0+0j], [1+0j, 0+1j, 0+0j], [0+0j, 0+0j, 0+0j] ]),           \
+            np.array([ [1+0j, 0+0j, 0+0j], [0+0j, -1+0j, 0+0j], [0+0j, 0+0j, 0+0j] ]),          \
+            np.array([ [0+0j, 0+0j, 1+0j], [0+0j, 0+0j, 0+0j], [1+0j, 0+0j, 0+0j] ]),           \
+            np.array([ [0+0j, 0+0j, 0-1j], [0+0j, 0+0j, 0+0j], [0+1j, 0+0j, 0+0j] ]),           \
+            np.array([ [0+0j, 0+0j, 0+0j], [0+0j, 0+0j, 1+0j], [0+0j, 1+0j, 0+0j] ]),           \
+            np.array([ [0+0j, 0+0j, 0+0j], [0+0j, 0+0j, 0-1j], [0+0j, 0+1j, 0+0j] ]),           \
+            (1 / math.sqrt(3)) * np.array([ [1+0j, 0+0j, 0+0j], [0+0j, 1+0j, 0+0j], [0+0j, 0+0j, -2+0j] ]),          \
+        ]
+
+
+def dft_gate(d):
+    omega = np.exp((- 2 * math.pi * imag_unit) / d )
+    W = np.eye(d, d, dtype='cfloat')
+    it = np.nditer(W, flags = ['multi_index'], op_flags = ['readwrite'])
+    for k in it:
+        i = it.multi_index[0]
+        j = it.multi_index[1]
+        W[i, j] = np.power(omega, i * j) / math.sqrt(d)
+        
+    return W
+
+
+def p_gate(d):
+    omega = np.exp((- 2 * math.pi * imag_unit) / d )
+    W = np.eye(d, d, dtype='cfloat')
+    for k in range(d):
+        W[k, k] = np.power(omega, ( k * (k - 1) ) // 2)
+
+    return W
+
+def sqrt_Z(d):
+    return np.diag( [ np.sqrt( np.exp(( -2 * math.pi * imag_unit * k) / d) ) for k in range(d) ] )
+
+def sqrt_sqrt_Z(d):
+    return np.sqrt(sqrt_Z(d))
+
+#implement later
+def SUM(d):
+   return np.eye(d, d, dtype='cfloat')
+
+def chp(d):
+    return [SUM(d), dft_gate(d), sqrt_Z(d)]
+
+def chp_plus_labels(d):
+    return [chp(d), ["C", "D", "Z"]]
+
+
+qubit_comp_basis =                                                      \
+        [                                                               \
+            np.array([[0+0j], [1+0j]]),                                 \
+            np.array([[1+0j], [0+0j]])                                  \
+        ]
+
