92803061ddf90e4b9e1a8781fa14df24a365668f
Steven Nguyen <03stevennguyen@gmail.com>
2023-09-16 22:16:55 -0400

    Implement basic global phase ignoring

--- compute_alg.py
+++ compute_alg.py
@@ -7,16 +7,25 @@ def qudit_stab(qudits, gates, rounds):
         for p in gates:
             for v in list(vectors.keys()):
                 vec = p * v
-                if vec not in vectors:
-                    vectors[vec] = vec
+                relPhase_vec = discard_global_phase_state(vec)
+                if relPhase_vec not in vectors:
+                    vectors[relPhase_vec] = relPhase_vec
 
     return vectors
 
-def normalize_global_phase(matrix):
-    max_element = max(matrix, key=abs)
-    if max_element != 0:
-        return simplify(matrix / max_element)
-    return matrix
+# Function to discard global phase of a state
+def discard_global_phase_state(mat):
+    mat = mat.as_mutable()  # Convert to mutable matrix
+    # Find the first non-zero element
+    for i in range(mat.rows):
+        if mat[i] != 0:  # Avoid division by zero
+            global_phase = arg(mat[i])
+            break
+    # Discard the global phase from all elements
+    for i in range(mat.rows):
+        if mat[i] != 0:  # Avoid division by zero
+            mat[i] = mat[i] * exp(-I * global_phase)
+    return mat.as_immutable()  # Convert back to immutable matrix if needed
 
 def qudit_comp_basis(d):
     return [ImmutableMatrix(d, 1, lambda i, j: 1 if i == k else 0) for k in range(d)]
@@ -28,7 +37,7 @@ if __name__ == "__main__":
     d = int(input("Dim: "))
     
     #gates = mats.pauli_matrices if d == 2 else mats.chp_3[:7]
-    
+
     #initialization 
     gates, symb = mats.chp_plus_labels(d)
     gate_seq = ["|" + str(k) + ">" for k in range(d)]
@@ -36,7 +45,9 @@ if __name__ == "__main__":
     stab = qudit_comp_basis(d)
     rounds_needed = 0
 
-    while rounds_needed < 1:
+    while rounds_needed < 2:
         stab = qudit_stab(stab, gates, 1)
         rounds_needed += 1
+    for keys in stab:
+        print(keys)
     print("Successfully terminated in " + str(rounds_needed) + " rounds.")
