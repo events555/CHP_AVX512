f86f09e9f7b259fd8cdbfea7d15c43184bd747c1
Steven Nguyen <03stevennguyen@gmail.com>
2023-11-12 22:06:30 -0500

    Add function to generate tensor products and mappings for SUM gate

--- test.py
+++ test.py
@@ -44,6 +44,7 @@ def generate_clifford(d):
                 break
 
     return P, W, SUM, S
+
 def generate_powers(X, Z, W, d):
     X_list = [(X**i).applyfunc(nsimplify) for i in range(1, d)]
     Z_list = [(Z**i).applyfunc(nsimplify) for i in range(1, d)]
@@ -51,12 +52,26 @@ def generate_powers(X, Z, W, d):
 
     return X_list, Z_list, W_list
 
+def generate_tensor_products(X, Z, I):
+    # Generate tensor products
+    XI = TensorProduct(X, I)
+    IX = TensorProduct(I, X)
+    ZI = TensorProduct(Z, I)
+    IZ = TensorProduct(I, Z)
+    XX = TensorProduct(X, X)
+    ZZ = TensorProduct(Z, Z)
+    XZ = TensorProduct(X, Z)
+    ZX = TensorProduct(Z, X)
+    Z_InvZ = TensorProduct(Z.inv(), Z)
+
+    return XI, IX, ZI, IZ, XX, ZZ, XZ, ZX, Z_InvZ
 # Usage
 d = 3  # Dimension
 X, Z = generate_paulis(d)
 P, W, SUM, S = generate_clifford(d)
 X_list, Z_list, W_list = generate_powers(X, Z, W, d)
 XZ_list = [(X_list[i-1] * Z_list[j-1]).applyfunc(nsimplify) for i in range(1, d) for j in range(1, d)]
+tensor_products = generate_tensor_products(X, Z, Matrix.eye(d))
 def generate_W_mapping(X_list, Z_list, W):
     mapping = {}
     for gate_list, gate_name in [(X_list, 'X'), (Z_list, 'Z')]:
@@ -110,6 +125,18 @@ def generate_S_mapping(X_list, Z_list, XZ_list, S):
                     mapping[f"{gate_name}^{i+1}"] = f"X^{k}*Z^{l}"
                     break
     return mapping
+
+def generate_SUM_mapping(tensor_products, SUM):
+    mapping = {}
+    tensor_product_names = ['X ⊗ I', 'I ⊗ X', 'Z ⊗ I', 'I ⊗ Z', 'X ⊗ X', 'Z ⊗ Z', 'X ⊗ Z', 'Z ⊗ X', 'Z^-1 ⊗ Z']
+    for i, tensor_product in enumerate(tensor_products):
+        conjugated_gate = SUM * tensor_product * SUM.inv()
+        conjugated_gate = conjugated_gate.applyfunc(nsimplify)
+        for j, tensor_product_prime in enumerate(tensor_products):
+            if conjugated_gate.equals(tensor_product_prime):
+                mapping[tensor_product_names[i]] = tensor_product_names[j]
+                break
+    return mapping
 # Usage
 W_mapping = generate_W_mapping(X_list, Z_list, W)
 for key, value in W_mapping.items():
@@ -118,4 +145,10 @@ for key, value in W_mapping.items():
 print("\nP mapping:")
 P_mapping = generate_P_mapping(X_list, Z_list, XZ_list, P)
 for key, value in P_mapping.items():
+    print(f"{key} -> {value}")
+
+
+print("\nSUM mapping:")
+SUM_mapping = generate_SUM_mapping(tensor_products, SUM)
+for key, value in SUM_mapping.items():
     print(f"{key} -> {value}")
