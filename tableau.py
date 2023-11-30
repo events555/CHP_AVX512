4b3254e84b83ab797b05a56e34542e90114736cb
Steven Nguyen <03stevennguyen@gmail.com>
2023-11-29 22:26:07 -0500

    Added phase

--- tableau.py
+++ tableau.py
@@ -6,7 +6,7 @@ class Tableau:
         self.num_qudits = num_qudits
         self.tableau = [QuditRegister("Stabilizer %d" % i, dimension, num_qudits) for i in range(num_qudits)]
         self.tableau.extend([QuditRegister("Destabilizer %d" % i, dimension, num_qudits) for i in range(num_qudits)])
-        
+        self.phase = [0 for _ in range(2*num_qudits)]
         for i in range(num_qudits):
             s = ['I'] * num_qudits  # Start with a list of 'I's
             s[i] = 'X'  # Replace the i-th element with 'X'
@@ -15,10 +15,11 @@ class Tableau:
             self.tableau[i + num_qudits].set(''.join(s))
 
     def __str__(self):
-        return "\n".join([str(qudit_register) for qudit_register in self.tableau])
+        return "\n".join([f"{str(qudit_register)} Phase: {self.phase[i]}" for i, qudit_register in enumerate(self.tableau)])
+
     
     def __getitem__(self, index):
         return self.tableau[index]
 
-table = Tableau(2, 2)
+table = Tableau(2, 4)
 print(table)
