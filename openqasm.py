afe9bd40d159335652856c0ec412c7172376cd04
Steven Nguyen <03stevennguyen@gmail.com>
2023-11-29 21:35:29 -0500

    Change to simplify_string func, support for d=2

--- openqasm.py
+++ openqasm.py
@@ -11,6 +11,14 @@ class Qudit:
             self.b = b
 
     def apply(self, gate, target=None):
+        if gate == "SUM" and target is not None:
+            for char in self.pauli_string:
+                if char == "X":
+                    target.pauli_string += "X"
+            for char in target.pauli_string:
+                if char == "Z":
+                    self.pauli_string += "Z"*(self.dimension-1)
+            return
         new_pauli_string = ""
         for char in self.pauli_string:
             if char == "X":
@@ -26,18 +34,10 @@ class Qudit:
                 elif gate == "S":
                     new_pauli_string += "Z"*self.b
         self.pauli_string = new_pauli_string
-        if gate == "SUM" and target is not None:
-            self.apply_SUM_gate(target)
-
-    def apply_SUM_gate(self, control_index, target_index):
-        control_qudit = self.qudits[control_index]
-        target_qudit = self.qudits[target_index]
-        if control_qudit.pauli_string == "X" and target_qudit.pauli_string == "I":
-            target_qudit.pauli_string = "X"
-        elif control_qudit.pauli_string == "I" and target_qudit.pauli_string == "Z":
-            control_qudit.pauli_string = "Z"*(self.dimension-1)
 
     def find_ab(self, d):
+        if d == 2:
+            return 1, 1
         for a in range(1, d):
             for b in range(1, d):
                 if (a * b) % d == 1 and not (a == 1 and b == 1):
@@ -69,7 +69,7 @@ class Gate:
     def __init__(self, name, qudit, target=None):
         self.name = name
         self.qudit_index = qudit
-        self.qudit_target = target
+        self.qudit_target_index = target
 
 
 class Circuit:
@@ -85,29 +85,28 @@ class Circuit:
 
     def simulate(self):
         for gate in self.gates:
-            qudit = self.qudit_register[gate.qudit_index]
             if gate.name == "SUM":
-                target = self.qudit_register[gate.target_index]
+                qudit = self.qudit_register[gate.qudit_index]
+                target = self.qudit_register[gate.qudit_target_index]
                 qudit.apply(gate.name, target)
+                target.pauli_string = self.simplify_string(target.pauli_string, target.dimension)
             else:
+                qudit = self.qudit_register[gate.qudit_index]
                 qudit.apply(gate.name)
-            qudit.pauli_string = self.replace_consecutive_chars(qudit.pauli_string, qudit.dimension)
+            qudit.pauli_string = self.simplify_string(qudit.pauli_string, qudit.dimension)
 
-    def replace_consecutive_chars(self, s, d):
+    def simplify_string(self, s, d):
         s = re.sub('X'*d, '' if len(s) > d else 'I', s)
         s = re.sub('Z'*d, '' if len(s) > d else 'I', s)
+        if 'X' in s or 'Z' in s:
+            s = s.replace('I', '')
         return s
 
-qr = QuditRegister("Input Array", 3, 3)
+qr = QuditRegister("Input Array", 2, 3)
 qc = Circuit(qr)
 qc.add_gate("R", 0)
 qc.add_gate("P", 1)
-qc.add_gate("S", 2)
-qc.add_gate("S", 0)
-qc.add_gate("S", 0)
-qc.add_gate("S", 2)
-qc.add_gate("R", 0)
-qc.add_gate("S", 0)
-print(qr)
+qc.add_gate("SUM", 0, 1)
+qc.add_gate("SUM", 1, 0)
 qc.simulate()
 print(qr)
