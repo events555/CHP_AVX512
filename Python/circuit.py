318fae26e32a82882fdc0087732d192e8eb38235
Steven Nguyen <03stevennguyen@gmail.com>
2023-12-13 20:45:07 -0500

    Refactor circuit and tableau classes

--- Python/circuit.py
+++ Python/circuit.py
@@ -1,62 +1,41 @@
 import re
-class Qudit:
-    def __init__(self, dimension, a=None, b=None):
-        self.dimension = dimension
-        self.pauli_string = "Z" # assume computational basis
-        if a is None or b is None:
-            self.a, self.b = self.find_ab(dimension)
-        else:
-            self.a = a
-            self.b = b
-    def set(self, pauli_string):
-        self.pauli_string = pauli_string
-    def apply(self, gate, target=None):
-        if gate == "SUM" and target is not None:
-            target.pauli_string += re.findall('X', self.pauli_string)
-            self.pauli_string += 'Z' * (self.dimension - 1) * len(re.findall('Z', target.pauli_string))
-            return
-
-        x_replacements = {'R': 'Z', 'P': 'XZ', 'S': 'X' * self.a}
-        z_replacements = {'R': 'X' * (self.dimension - 1), 'P': 'Z', 'S': 'Z' * self.b}
-
-        self.pauli_string = re.sub('X', lambda match: x_replacements[gate], self.pauli_string)
-        self.pauli_string = re.sub('Z', lambda match: z_replacements[gate], self.pauli_string)
-
-    def find_ab(self, d):
-        if d == 2:
-            return 1, 1
-        for a in range(1, d):
-            for b in range(1, d):
-                if (a * b) % d == 1 and not (a == 1 and b == 1):
-                    return a, b
-        return None, None
 
 class QuditRegister:
-    def __init__(self, name, dimension, size):
+    def __init__(self, name, dimension, num_qudits):
         self.name = name
         self.dimension = dimension
-        self.size = size
-        a, b = self.find_ab(dimension)
-        self.qudits = [Qudit(dimension, a, b) for _ in range(size)]
+        self.num_qudits = num_qudits
+        self.pauli_product = ['I' for _ in range(num_qudits)]
 
     def __getitem__(self, index):
-        return self.qudits[index]
+        return self.pauli_product[index]
 
     def __str__(self):
-            tensor_product = "\u2297 ".join([qudit.pauli_string for qudit in self.qudits])
+            tensor_product = "\u2297 ".join(self.pauli_product)
             return f"{self.name}: {tensor_product}"
 
-    def set(self, pauli_string):
-        for i, qudit in enumerate(self.qudits):
-            qudit.set(pauli_string[i])
+    def set(self, pauli, index=None):
+        print(pauli.split())
+        if index is None:
+            pauli_split = pauli.split()
+            for i in range(self.num_qudits):
+                self.pauli_product[i] = pauli_split[i]
+                simplified = self.simplify_string(self.pauli_product[i], self.dimension)
+                if simplified is not None:
+                    self.pauli_product = simplified
+        else:
+            self.pauli_product[index] = pauli
+            simplified = self.simplify_string(self.pauli_product[index], self.dimension)
+            if simplified is not None:
+                self.pauli_product = simplified
 
-    def find_ab(self, d):
-        for a in range(1, d):
-            for b in range(1, d):
-                if (a * b) % d == 1 and not (a == 1 and b == 1):
-                    return a, b
-        return None, None
 
+        
+    def simplify_string(self, s, d):
+        s = re.sub('X'*d, '' if len(s) > d else 'I', s)
+        s = re.sub('Z'*d, '' if len(s) > d else 'I', s)
+        if 'X' in s or 'Z' in s:
+            s = s.replace('I', '')
 
 class Gate:
     def __init__(self, name, qudit, target=None):
@@ -64,47 +43,20 @@ class Gate:
         self.qudit_index = qudit
         self.qudit_target_index = target
 
-
 class Circuit:
     def __init__(self, qudit_register):
         self.qudit_register = qudit_register
         self.gates = []
-
     def add_gate(self, gate_name, qudit_index, target_index=None):
-        if gate_name not in ["R", "P", "SUM", "S"]:
+        if gate_name not in ["R", "P", "SUM"]:
             raise ValueError(f"Invalid gate: {gate_name}")
+        elif gate_name == "SUM" and target_index not in range(self.qudit_register.num_qudits):
+            raise ValueError(f"Invalid target index: {target_index}")
+        elif qudit_index not in range(self.qudit_register.num_qudits):
+            raise ValueError(f"Invalid qudit index: {qudit_index}")
         gate = Gate(gate_name, qudit_index, target_index)
         self.gates.append(gate)
-
-    def simulate(self):
-        if self.qudit_register.dimension==2:
-            self.qudit_register = chp.simulate(self.qudit_register, self.gates) 
-        else:
-            for gate in self.gates:
-                if gate.name == "SUM":
-                    qudit = self.qudit_register[gate.qudit_index]
-                    target = self.qudit_register[gate.qudit_target_index]
-                    qudit.apply(gate.name, target)
-                    target.pauli_string = self.simplify_string(target.pauli_string, target.dimension)
-                else:
-                    qudit = self.qudit_register[gate.qudit_index]
-                    qudit.apply(gate.name)
-                qudit.pauli_string = self.simplify_string(qudit.pauli_string, qudit.dimension)
     def __str__(self):
         gate_list = "\n".join([f"Gate: {gate.name}, Qudit Index: {gate.qudit_index}, Target Index: {gate.qudit_target_index}" for gate in self.gates])
         return f"Circuit:\n{gate_list}"
-    def simplify_string(self, s, d):
-        s = re.sub('X'*d, '' if len(s) > d else 'I', s)
-        s = re.sub('Z'*d, '' if len(s) > d else 'I', s)
-        if 'X' in s or 'Z' in s:
-            s = s.replace('I', '')
-        return s
-
-qr = QuditRegister("Input Array", 2, 3)
-qc = Circuit(qr)
-qc.add_gate("R", 1)
-qc.add_gate("P", 1)
-qc.add_gate("P", 1)
-qc.add_gate("SUM", 0, 1)
-qc.simulate()
-print(qr)
+    
--- Python/tableau.py
+++ Python/tableau.py
@@ -1,4 +1,5 @@
-from circuit import Qudit, QuditRegister, Gate, Circuit
+from circuit import QuditRegister, Circuit
+import re
 
 class Tableau:
     def __init__(self, dimension, num_qudits):
@@ -10,16 +11,45 @@ class Tableau:
         for i in range(num_qudits):
             s = ['I'] * num_qudits  # Start with a list of 'I's
             s[i] = 'X'  # Replace the i-th element with 'X'
-            self.tableau[i].set(''.join(s))
+            self.tableau[i].set(' '.join(s))
             s[i] = 'Z'
-            self.tableau[i + num_qudits].set(''.join(s))
+            self.tableau[i + num_qudits].set(' '.join(s))
 
     def __str__(self):
         return "\n".join([f"{str(qudit_register)} Phase: {self.phase[i]}" for i, qudit_register in enumerate(self.tableau)])
-
-    
     def __getitem__(self, index):
         return self.tableau[index]
+    def conjugate(self, qudit_index, qudit_target_index, gate, stabilizer):
+        # Conjugate's According to Gottesman's Higher Dimensional Mappings
+        # https://arxiv.org/abs/quant-ph/9802007
+        if qudit_target_index == None:
+            if gate == "R":
+                replacements = {'X': 'Z', 'Z': 'X' * (self.dimension - 1)}
+                stabilizer.pauli_product[qudit_index] = ''.join([replacements.get(c, c) for c in stabilizer.pauli_product[qudit_index]])
+            elif gate == "P":
+                replacements = {'X': 'XZ', 'Z': 'Z'}
+                stabilizer.pauli_product[qudit_index] = ''.join([replacements.get(c, c) for c in stabilizer.pauli_product[qudit_index]])
+        else:
+            raise ValueError(f"Two qudit gates are not supported yet.")
+class Program:
+    def __init__(self, tableau, circuit):
+        self.stabilizer_tableau = tableau
+        self.circuit = circuit
+    def simulate(self):
+        for stabilizer in self.stabilizer_tableau.tableau:
+            for gate in self.circuit.gates:
+                self.stabilizer_tableau.conjugate(gate.qudit_index, gate.qudit_target_index, gate.name, stabilizer)
 
 table = Tableau(2, 4)
+print(table)
+
+qudit_register = QuditRegister("Qudit Register", 2, 4)
+circuit = Circuit(qudit_register)
+circuit.add_gate("R", 0)
+circuit.add_gate("P", 1)
+#circuit.add_gate("SUM", 2, 3)
+print(circuit)
+
+program = Program(table, circuit)
+program.simulate()
 print(table)
